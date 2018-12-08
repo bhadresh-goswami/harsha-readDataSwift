9517ea4362e8f8dfc6f90a946a6cba24df41f172
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-08 05:38:20 -0800

    first swift program console

--- readDataSwift/main.swift
+++ readDataSwift/main.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-print("Hello, World!")
+var a = 0//by default it takes Int data type
+var b:Int32//you can specifide the data type
+
+//print("Enter value for a:\t")
+print("\nEnter Value in a:\t", separator: "", terminator: "")
+
+
+a = Int(readLine()!)!
+
+//print("Enter value for b:\t")
+
+print("\nEnter Value in b:\t", separator: "", terminator: "")
+b = Int32(Int(readLine()!)!)
+
+//! will indicates that the Value is not optional
+//you must, must have to assign value.
+
+var c = a + Int(b)
+
+print("The value of a \(a) and b \(b) : addition is \(c)")
 
